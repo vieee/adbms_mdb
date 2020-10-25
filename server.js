f31921dbb438d8dede0583c43778705dba5522c6
Deepak <dgyadv1782000@gmail.com>
2020-10-26 02:34:01 +0530

    working app

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const Article = require('./models/article')
-const articleRouter = require('./routes/articles')
-const methodOverride = require('method-override')
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const Article = require("./models/article");
+const articleRouter = require("./routes/articles");
+const methodOverride = require("method-override");
+const app = express();
 
-mongoose.connect('mongodb://localhost/blog', {
-  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
-})
+const MONGO_URI =
+  "mongodb+srv://Deepak:MongoDB%401234@cluster0-g1rdx.gcp.mongodb.net/mern-crud?retryWrites=true&w=majority";
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
-app.use(methodOverride('_method'))
 
-app.get('/', async (req, res) => {
-  const articles = await Article.find().sort({ createdAt: 'desc' })
-  res.render('articles/index', { articles: articles })
-})
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 
-app.use('/articles', articleRouter)
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: "desc" });
+  res.render("articles/index", { articles: articles });
+});
 
-app.listen(5000)
+app.use("/articles", articleRouter);
+
+mongoose
+  .connect(MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  .then((_result) => {
+    app.listen(5000);
+    console.log("hello")
+  })
+  .catch((err) => console.log(err));
